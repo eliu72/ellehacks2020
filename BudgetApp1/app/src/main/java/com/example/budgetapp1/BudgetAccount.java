311f9a1055c5a49c98bf05e672b3d83974c534b3
eliu22 <elaineliu7g@gmail.com>
2020-02-01 20:47:20 -0500

    new

--- BudgetApp1/app/src/main/java/com/example/budgetapp1/BudgetAccount.java
+++ BudgetApp1/app/src/main/java/com/example/budgetapp1/BudgetAccount.java
@@ -4,29 +4,94 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import java.util.ArrayList;
 
 public class BudgetAccount<response> extends AppCompatActivity {
+    private ArrayList<Card> mExampleList;
+
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
+    private ExampleAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private Button buttonInsert;
+    private Button buttonRemove;
+   // private EditText editTextInsert;
+   // private EditText editTextRemove;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget_account);
 
-        ArrayList<Card> cardList = new ArrayList<>();
-        cardList.add(new Card(R.drawable.ic_credit_card_black_24dp, "Gong Cha", /*Budget: $15.00\nSpent: $6.76       */"Remaining: $8.43"));
-        cardList.add(new Card(R.drawable.ic_credit_card_black_24dp,"No Frills", /*Budget: $50.00\nSpent: $22.32       */"Remaining: $27.68"));
-        cardList.add(new Card(R.drawable.ic_credit_card_black_24dp,"Tim Horton's", /*Budget: $40.00\nSpent: $17.15       */"Remaining: $22.85"));
+        createExampleList();
+        buildRecycleView();
+
+        buttonInsert = findViewById(R.id.button_insert);
+        //buttonRemove = findViewById(R.id.button_remove);
+        //editTextInsert = findViewById(R.id.edittext_insert);
+        //editTextRemove = findViewById(R.id.edittext_remove);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = mExampleList.size();
+                insertItem(position);
+            }
+        });
+
+       /* buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = mExampleList.size();
+                insertItem(position);
+
+            }
+        });*/
+
+    }
+
+    public void insertItem(int position){
+        mExampleList.add(new Card(R.drawable.ic_credit_card_black_24dp, "Store Name", "Remaining: $0.00"));
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void changeItem(int position, String text){
+        mExampleList.get(position).changeText1(text);
+        mAdapter.notifyItemChanged(position);
+
+    }
+
+    /*public void removeItem(int position){
+
+
+    }*/
+
+    public void createExampleList(){
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new Card(R.drawable.ic_credit_card_black_24dp, "Gong Cha", /*Budget: $15.00\nSpent: $6.76       */"Remaining: $8.43"));
+        mExampleList.add(new Card(R.drawable.ic_credit_card_black_24dp,"No Frills", /*Budget: $50.00\nSpent: $22.32       */"Remaining: $27.68"));
+        mExampleList.add(new Card(R.drawable.ic_credit_card_black_24dp,"Tim Horton's", /*Budget: $40.00\nSpent: $17.15       */"Remaining: $22.85"));
+    }
+
+    public void buildRecycleView(){
 
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(cardList);
+        mAdapter = new ExampleAdapter(mExampleList);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
 
+        mAdapter.setOnItemClickListener(new ExampleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                changeItem(position, "Clicked");
+            }
+        });
     }
 }
--- BudgetApp1/app/src/main/java/com/example/budgetapp1/ExampleAdapter.java
+++ BudgetApp1/app/src/main/java/com/example/budgetapp1/ExampleAdapter.java
@@ -1,5 +1,6 @@
 package com.example.budgetapp1;
 
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +15,41 @@ import java.util.ArrayList;
 public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
 
     private ArrayList<Card> mCardList;
+    private OnItemClickListener mListener;
+
+    public interface  OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+
+    }
     public static class ExampleViewHolder extends RecyclerView.ViewHolder{
 
         public ImageView mImageView;
         public TextView mTextView1;
         public TextView mTextView2;
 
-        public ExampleViewHolder(View itemView){
+        public ExampleViewHolder(View itemView, final OnItemClickListener listener){
             super(itemView);
             mImageView = itemView.findViewById(R.id.imageView);
             mTextView1 = itemView.findViewById(R.id.textView);
             mTextView2 = itemView.findViewById(R.id.textView2);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+
+                    }
+                }
+
+            });
         }
     }
 
@@ -36,7 +61,7 @@ public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleV
     @Override
     public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card1, parent, false);
-        ExampleViewHolder evh = new ExampleViewHolder(v);
+        ExampleViewHolder evh = new ExampleViewHolder(v, mListener);
         return evh;
     }
 
